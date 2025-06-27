91a7c7e9b6a6c74eb27e1c95784f5b61aa2713b8
Tom Seddon <->
2025-06-28 00:11:02 +0100

    Add test program for SDL3 TLS-related leak.

---
+++ README.md
@@ -0,0 +1,21 @@
+Test program for SDL3 TLS-related leak on Windows.
+
+Tested using Visual Studio 2019 on Windows 10 22H2 (OS Build
+19045.5965).
+
+To build, first do this from working copy:
+
+    mkdir build
+	cd build
+	cmake -G "Visual Studio 16 2019" -A x64 ..
+	
+(If you installed CMake as part of VS2019, it'll include a suitable
+cmake. Path on my PC was this: `C:\Program Files (x86)\Microsoft
+Visual
+Studio\2019\Professional\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe`)
+	
+Then load `build/sdl3_tls_leak.sln` into VS2019.
+
+Then build Debug x64 and run.
+
+Note memory leaks in output window.
---
+++ src/sdl3_tls_leak.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <crtdbg.h>
+#include <string>
+
+static int StartupError(const std::string &what) {
+    std::string message = what + " failed: " + SDL_GetError();
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Startup failed", message.c_str(), nullptr);
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    (void)argc, (void)argv;
+
+    _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+
+    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+        return StartupError("SDL_Init");
+
+    SDL_Window *window = SDL_CreateWindow("sdl3_tls_leak", 1280, 720, SDL_WINDOW_RESIZABLE);
+    if (!window)
+        return StartupError("SDL_CreateWindow");
+
+    SDL_DestroyWindow(window), window = nullptr;
+
+    SDL_CleanupTLS();
+
+    SDL_Quit();
+
+    return 0;
+}
